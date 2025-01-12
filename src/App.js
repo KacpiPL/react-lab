0903324736d82d0d42231fab79764b3545ee3daa
Kacper <kacpergruca@gmail.com>
2025-01-12 15:06:44 +0100

    Add ul list of titles

--- src/App.js
+++ src/App.js
@@ -12,13 +12,24 @@ function App() {
       message = "Tytuł jest za długi, nikt tego nie zapamięta.";
   }
 
+  const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+  ];
+
   function handleChange(event) {
     setTitle(event.target.value);
   }
 
   return (
     <div>
-      <h1>My favourite movies for today</h1>
+      <h1>My favourite movies to watch</h1>
+      <h2>Titles</h2>
+      <ul>
+          {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+      </ul>
       <h2>My favourite movie for today is {title}</h2>
       {title.length > 0 && <div>{message}</div>}
       <input type="text" value={title} onChange={handleChange}/>
